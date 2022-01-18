7091b7f487eb40f7c01925a387dcc4d6ad7c7ea0
daichigoto <daichigoto@gmail.com>
2022-01-18 13:37:47 +0900

    updated

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,11 @@
-/* #ifndef UNICODE */
-/* #define UNICODE */
-/* #endif */ 
-
 /*
  * Reference:
  *    https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
  */
 #include <windows.h>
+#include <tchar.h>
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
-  return MessageBox(NULL, "新大陸へようこそ!", "はじめてのウィンドウプログラミング", 0);
+  return MessageBox(NULL, _T("新大陸へようこそ!"), _T("はじめてのウィンドウプログラミング"), 0);
 }
