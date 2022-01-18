c0da77028da7a0484d9b3f1676b4450643c375e8
daichigoto <daichigoto@gmail.com>
2022-01-18 13:14:15 +0900

    updated

--- helloworld.c
+++ helloworld.c
@@ -10,5 +10,5 @@
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
-  return MessageBox(NULL, TEXT("新大陸へようこそ!"), TEXT("はじめてのウィンドウプログラミング"), MB_OK);
+  return MessageBox(NULL, "新大陸へようこそ!", "はじめてのウィンドウプログラミング", 0);
 }
